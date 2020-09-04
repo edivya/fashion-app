eb86fc0e2c9122ccad899de61e4577df5b8005b4
divya <enjamuridivya@gmail.com>
2020-09-04 15:13:29 -0700

    added anchor tag page

--- src/containers/Header/index.js
+++ src/containers/Header/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 const Header = (props)=> {
     return(
-        <header>
-            <nav>
+        <header className="header">
+            <nav className="headerMenu">
                 <a href= "#">Home</a>
                 <a href= "#">About Us</a>
                 <a href= "#">Contact Us</a>
             </nav>
+            <div>
+                social Media links
+            </div>
         </header>
     )
 }
