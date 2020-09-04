27d1712f6e9be92ba948718dbd935d73b7afad3f
divya <enjamuridivya@gmail.com>
2020-09-04 16:21:08 -0700

    changes modified on hero page

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,9 +1,13 @@
 import React from "react";
 import './style.css';
+import Card from "../UI/Card";
+import Logo from "../Logo"
 const Hero =(props) => {
     return (
         <div>
-            
+           <Card>
+             <Logo/>  
+           </Card>
         </div>
     )
 }
