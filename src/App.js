eecf91f59fd9d879947a73c22e96825e69f91b99
divya <enjamuridivya@gmail.com>
2020-09-04 15:47:00 -0700

    added header page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
-
 import './App.css';
 import Home from './containers/Home';
-import Header from './containers/Header';
+import Header from './components/Header';
+import Hero from './components/Hero'
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Hero/>
       <Home/>
     </div>
   );
---
+++ src/components/Hero/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './style.css';
+const Hero =(props) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+export default Hero;
