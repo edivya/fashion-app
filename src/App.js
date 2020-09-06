a9a35d539626335da50f326c4b6b623ece83ecf8
divya <enjamuridivya@gmail.com>
2020-09-06 16:35:17 -0700

    added source pages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import './App.css';
-import Home from './containers/Home';
-import Header from './components/Header';
-import Hero from './components/Hero'
-
-function App() {
-  return (
-    <div className="App">
-      <Header/>
-      <Hero/>
-      <Home/>
-    </div>
-  );
+import React, { Component } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Route, Switch } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import ProductList from "./components/ProductList";
+import Details from "./components/Details";
+import Default from "./components/Default";
+import Cart from "./components/Cart";
+import Modal from "./components/Modal";
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={ProductList} />
+          <Route path="/details" component={Details} />
+          <Route path="/cart" component={Cart} />
+          <Route component={Default} />
+        </Switch>
+        <Modal />
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
