83b1182acbef2cec81de8987371b6fca170bfb27
divya <enjamuridivya@gmail.com>
2020-09-06 17:51:19 -0700

    changes made

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,11 +4,11 @@ export const ButtonContainer = styled.button`
   text-transform: capitalize;
   font-size: 1.4rem;
   background: transparent;
-  border: 0.05rem solid var(--lightBlue);
+  border: 0.05rem solid var(--white);
   border-color: ${props =>
-    props.cart ? "var(--mainYellow)" : "var(--lightBlue)"};
+    props.cart ? "var(--mainYellow)" : "var(--white)"};
   color: var(--lightBlue);
-  color: ${props => (props.cart ? "var(--mainYellow)" : "var(--lightBlue)")};
+  color: ${props => (props.cart ? "var(--mainYellow)" : "var(--white)")};
   border-radius: 0.5rem;
   padding: 0.2rem 0.5rem;
   outline-color: red;
@@ -19,7 +19,7 @@ export const ButtonContainer = styled.button`
   &:hover {
     background: var(--lightBlue);
     background: ${props =>
-      props.cart ? "var(--mainYellow)" : "var(--lightBlue)"};
+      props.cart ? "var(--mainYellow)" : "var(--white)"};
     color: var(--mainBlue);
   }
   &:focus {
