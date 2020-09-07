12ed9e9e0cfa841aed3ea1d51ac7c837d9028551
divya <enjamuridivya@gmail.com>
2020-09-07 00:14:15 -0700

    images added to components page

---
+++ build/precache-manifest.0e7ba604e6900d0483c4beac794b10a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec277109f53ba7c26cc1",
+    "url": "/static/css/main.5583e5c6.chunk.css"
+  },
+  {
+    "revision": "ec277109f53ba7c26cc1",
+    "url": "/static/js/main.ec277109.chunk.js"
+  },
+  {
+    "revision": "db8ae2c4b65eb4a82acd",
+    "url": "/static/css/1.7bb6c7db.chunk.css"
+  },
+  {
+    "revision": "db8ae2c4b65eb4a82acd",
+    "url": "/static/js/1.db8ae2c4.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f6707bcc34d28dddfabdc6a2b0e60d95",
+    "url": "/static/media/logo.f6707bcc.jpg"
+  },
+  {
+    "revision": "80a698ecb4b5cf79e597f266b1ec20c8",
+    "url": "/index.html"
+  }
+];
